bbc99d3d7fecf8e6e16bb75d9ec48ef0a5f1378c
zannatreya <zannatulbintabahar@yahoo.com>
2022-03-30 09:43:54 +0600

    first commit

---
+++ src/Components/Restaurant/Restaurant.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Restaurant = () => {
+    return (
+        <div>
+            <h1>this is restaurant</h1>
+        </div>
+    );
+};
+
+export default Restaurant;
