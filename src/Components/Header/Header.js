8c79d5b1a1899074ea5be13e58f41d8da3b51404
zannatreya <zannatulbintabahar@yahoo.com>
2022-03-30 17:55:01 +0600

    search updated

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
     return (
         <div>
+            <h1>Welcome to my restaurant</h1>
+            <nav>
+                <CustomLink to="/">Home</CustomLink>
+                <CustomLink to="/restaurant">Restaurant</CustomLink>
+                <CustomLink to="/about">About</CustomLink>
 
+            </nav>
         </div>
     );
 };
--- src/Components/Restaurant/Restaurant.js
+++ src/Components/Restaurant/Restaurant.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import Meal from "../Meal/Meal";
+import './restaurant.css'
 
 const Restaurant = () => {
+    const [searchText, setSearchText] = useState('');
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setMeals(data.meals));
+    }, [searchText])
+
+    const searchFood = e => {
+        setSearchText(e.target.value);
+    }
     return (
         <div>
-            <h1>this is restaurant</h1>
+            <h2>Find the food you want</h2>
+            <input onChange={searchFood} type="text" name="" id="" />
+            <br />
+            <h3>Result found: {meals.length}</h3>
+            <div className='meals-container'>
+                {
+                    meals.map(meal => <Meal key={meal.idMeal} meal={meal}></Meal>)
+                }
+            </div>
         </div>
     );
 };
